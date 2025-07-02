720f256dec534670f10ee717e5fc79c865cb76a0
douaoouria rassim <162766373+Rassimdou@users.noreply.github.com>
2025-07-02 21:58:46 +0100

    first push

---
+++ internal/ping.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func RunPingTest() {
+	start := time.Now()
+	_, err := http.Get("https://www.google.com")
+	if err != nil {
+		fmt.Println("Error making GET request:", err)
+	}
+	elapsed := time.Since(start)
+	fmt.Println("Ping :", elapsed.Milliseconds(), "ms")
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("the server is running on port 8080")
+
+	RunPingTest()
+
+}
